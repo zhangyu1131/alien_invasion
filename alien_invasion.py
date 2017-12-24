b89ebb8a8bb78b862f876e1d026c3c7c4c1f07a4
Rain <923446353@qq.com>
2017-12-24 15:24:02 +0800

    game screen init and create a screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,2 +1,17 @@
+import sys
 import pygame
+
+def run_game():
+    #初始化游戏并创建一个屏幕对象
+    pygame.init()
+    screen=pygame.display.set_mode((1200,600))
+    pygame.display.set_caption("Alien Invasion")
+    while True:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                sys.exit()
+
+        pygame.display.flip()
+
+run_game()
 
